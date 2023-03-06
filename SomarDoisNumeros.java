9141b0594ad1dce50309693c9bd9483d6b8b3554
hannachaves <111326235+hannachaves@users.noreply.github.com>
2023-03-06 14:32:03 -0300

    Exercício 01

--- SomarDoisNumeros.java
+++ SomarDoisNumeros.java
@@ -1,23 +1,22 @@
-import java.util.*;
+ import java.util.*;
 
 class SomarDoisNumeros {
 	public static Scanner sc = new Scanner(System.in);
 	
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		// declaracao de variaveis
 		int num1, num2, soma;
 		
 		// leituras
-		System.out.println("Digite um numero");
+		System.out.println("Digite um numero: ");
 		num1 = sc.nextInt();
-		System.out.println("Digite outro numero");
+		System.out.println("Digite outro numero: ");
 		num2 = sc.nextInt();
 		
 		// somar
 		soma = num1 + num2;
 		
-		// mostrar na tela
+		// mostrar
 		System.out.println("Soma:" +soma);
 	}
-
 }
